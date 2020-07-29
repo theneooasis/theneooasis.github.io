e71c7795de4adbd8f11dd91b27e9757102e5a738
kimchanjung <mogomezwai@gmail.com>
2020-07-29 16:08:24 +0900

    변경

--- publish.sh
+++ publish.sh
@@ -2,7 +2,6 @@ cp .gitignore _site
 git checkout source
 git branch -D master
 git checkout -b master
-cp .gitignore _site
 git filter-branch --subdirectory-filter _site/ -f
 git push --all
 git checkout source
